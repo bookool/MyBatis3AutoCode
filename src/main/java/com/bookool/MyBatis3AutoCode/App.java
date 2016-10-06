2d0fde64cd805a4665b2d3296ba5741e2d483de6
关勇 <msvbprogrammer@hotmail.com>
2016-10-06 16:01:41 +0800

    创建

---
+++ src/main/java/com/bookool/MyBatis3AutoCode/App.java
@@ -0,0 +1,358 @@
+package com.bookool.MyBatis3AutoCode;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.regex.*;
+
+import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
+
+import java.text.SimpleDateFormat;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.Node;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+import org.dom4j.Attribute;
+import org.dom4j.io.SAXReader;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world!");
+		String LogFileName = System.getProperty("user.dir") + "/autocode.log";
+		File logfile = new File(LogFileName);
+		if (!logfile.exists())
+		{
+			try
+			{
+				logfile.createNewFile();
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+				return;
+			}
+		}
+		logfile = null;
+		System.out.println("日志文件：" + LogFileName);
+		AppLog.WriteLog("Hello world!");
+
+		if (StartPro(args))
+		{
+			AppLog.WriteLog("结束！全部成功!\r\n");
+		}
+		else
+		{
+			AppLog.WriteLog("结束！有错误发生!\r\n", true);
+		}
+	}
+
+	/**
+	 * 开始自动生成
+	 * 
+	 * @param args
+	 *            命令参数
+	 * @return 是否全部成功
+	 */
+	private static boolean StartPro(String[] args)
+	{
+		AppLog.WriteLog("检查参数1：XML配置文件");
+		if (args.length != 1)
+		{
+			AppLog.WriteLog("参数1错误！", true);
+			return false;
+		}
+		AppLog.WriteLog("解析文件：" + args[0]);
+		String PackageName;
+		String TableNamePrefixion;
+		String TableScriptDir;
+		String ModelDir;
+		String DaoDir;
+		String ServiceDir;
+		String ServiceImplDir;
+		try
+		{
+			SAXReader saxReader = new SAXReader();
+			Document doc = saxReader.read(new File(args[0]));
+			Element zroot = doc.getRootElement();
+			PackageName = VNode(zroot, "PackageName");
+			TableNamePrefixion = VNode(zroot, "TableNamePrefixion");
+			TableScriptDir = VNode(zroot, "TableScriptDir");
+			ModelDir = VNode(zroot, "ModelDir");
+			DaoDir = VNode(zroot, "DaoDir");
+			ServiceDir = VNode(zroot, "ServiceDir");
+			ServiceImplDir = VNode(zroot, "ServiceImplDir");
+		}
+		catch (Exception e)
+		{
+			AppLog.WriteLog("发生错误：" + e.getMessage(), true);
+			return false;
+		}
+		File[] fs = new File(TableScriptDir).listFiles();
+		for (int i = 0; i < fs.length; i++)
+		{
+			if (fs[i].getName().matches(".+(?i)\\.sql$"))
+			{
+				AppLog.WriteLog("分析数据库脚本：" + fs[i].getName());
+				String tablestr = "";
+				try
+				{
+					FileReader fr = new FileReader(fs[i]);
+					BufferedReader br = new BufferedReader(fr);
+					try
+					{
+						String rline = br.readLine();
+						while (rline != null)
+						{
+							boolean canread = false;
+							StringBuilder sbf = new StringBuilder();
+							while (rline != null)
+							{
+								if (rline.matches("(?i)\\s*CREATE\\s*TABLE\\s*`[^`]+`\\s*\\(.*"))
+								{
+									canread = true;
+								}
+								if (canread)
+								{
+									sbf.append(rline);
+								}
+								if (rline.matches(".*?\\).*?(?i)COMMENT\\s*=\\s*'[^']+'.*?;.*?$"))
+								{
+									rline = br.readLine();
+									break;
+								}
+								rline = br.readLine();
+							}
+							tablestr = sbf.toString();
+							if (tablestr.matches("(?i)\\s*CREATE\\s*TABLE\\s*`[^`]+`\\s*\\(.*\\).*COMMENT\\s*=\\s*'[^']+'.*;.*$"))
+							{
+								mytable zt = GetTable(tablestr, PackageName, TableNamePrefixion, TableScriptDir,
+										ModelDir, DaoDir, ServiceDir, ServiceImplDir);
+								if (zt != null)
+								{
+									if (zt.getFields().isEmpty() && zt.getPriFields().isEmpty())
+									{
+										AppLog.WriteLog("发生错误：[" + zt.getTableName() + "] 表中没有字段！", true);
+									}
+									else
+									{
+										BuildCode.BuildModel(zt);
+										AppLog.WriteLog("成功生成了 model/" + zt.getTableName() + ".java 文件。");
+										BuildCode.BuildDao(zt);
+										AppLog.WriteLog("成功生成了 dao/" + zt.getTableName() + "Mapper.java 文件。");
+										BuildCode.BuildDaoXML(zt);
+										AppLog.WriteLog("成功生成了 dao/" + zt.getTableName() + "Mapper.xml 文件。");
+										BuildCode.BuildService(zt);
+										AppLog.WriteLog("成功生成了 service/" + zt.getTableName() + "Service.java 文件。");
+										BuildCode.BuildServiceImpl(zt);
+										AppLog.WriteLog(
+												"成功生成了 service/impl/" + zt.getTableName() + "ServiceImpl.java 文件。");
+									}
+								}
+							}
+						}
+						br.close();
+					}
+					catch (IOException e)
+					{
+						// TODO 自动生成的 catch 块
+						e.printStackTrace();
+						AppLog.WriteLog("发生错误：" + e.getMessage(), true);
+					}
+					catch (Exception e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						AppLog.WriteLog("发生错误：" + e.getMessage(), true);
+					}
+				}
+				catch (FileNotFoundException e)
+				{
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+					AppLog.WriteLog("发生错误：" + e.getMessage(), true);
+				}
+
+			}
+		}
+		return true;
+
+	}
+
+	/**
+	 * 将脚本字符串转为table对象
+	 */
+	private static mytable GetTable(String TabStr, String PackageName, String TableNamePrefixion, String TableScriptDir,
+			String ModelDir, String DaoDir, String ServiceDir, String ServiceImplDir) throws Exception
+	{
+		Matcher zm = Pattern
+				.compile("\\s*CREATE\\s*TABLE\\s*`" + TableNamePrefixion
+						+ "([^`]+)`\\s*\\((.*)\\).*COMMENT\\s*=\\s*'([^']+)'.*;.*", Pattern.CASE_INSENSITIVE)
+				.matcher(TabStr);
+		if (zm.find())
+		{
+			String TableName = zm.group(1).trim();
+			String TableComment = zm.group(3).trim();
+			AppLog.WriteLog("找到数据表：" + TableName);
+			boolean fe = false;
+			File tfile = new File(ModelDir + "/" + TableName + ".java");
+			if (tfile.exists())
+			{
+				AppLog.WriteLog(tfile.getAbsolutePath() + " 已经存在。", true);
+				fe = true;
+			}
+			tfile = new File(DaoDir + "/" + TableName + "Mapper.java");
+			if (tfile.exists())
+			{
+				AppLog.WriteLog(tfile.getAbsolutePath() + " 已经存在。", true);
+				fe = true;
+			}
+			tfile = new File(DaoDir + "/" + TableName + "Mapper.xml");
+			if (tfile.exists())
+			{
+				AppLog.WriteLog(tfile.getAbsolutePath() + " 已经存在。", true);
+				fe = true;
+			}
+			tfile = new File(ServiceDir + "/" + TableName + "Service.java");
+			if (tfile.exists())
+			{
+				AppLog.WriteLog(tfile.getAbsolutePath() + " 已经存在。", true);
+				fe = true;
+			}
+			tfile = new File(ServiceImplDir + "/" + TableName + "ServiceImpl.java");
+			if (tfile.exists())
+			{
+				AppLog.WriteLog(tfile.getAbsolutePath() + " 已经存在。", true);
+				fe = true;
+			}
+			if (fe)
+			{
+				AppLog.WriteLog("代码已经存在，" + TableName + " 不再生成。", true);
+				return null;
+			}
+			else
+			{
+				String TableCon = zm.group(2).trim();
+				List<String> prilist = new ArrayList<String>();
+				zm = Pattern.compile(".*PRIMARY\\s+KEY\\s*\\(([^\\(\\)]+)\\).*").matcher(TableCon);
+				if (zm.find())
+				{
+					zm = Pattern.compile("\\s*`\\s*([^`]+?)\\s*`\\s*").matcher(zm.group(1));
+					while (zm.find())
+					{
+						prilist.add(zm.group(1).trim());
+					}
+				}
+				mytable zt = new mytable();
+				zt.setPackageName(PackageName);
+				zt.setTableNamePrefixion(TableNamePrefixion);
+				zt.setTableScriptDir(TableScriptDir);
+				zt.setModelDir(ModelDir);
+				zt.setDaoDir(DaoDir);
+				zt.setServiceDir(ServiceDir);
+				zt.setServiceImplDir(ServiceImplDir);
+				zt.setTableName(TableName);
+				zt.setTableComment(TableComment);
+				zt.setFields(new ArrayList<myfield>());
+				zt.setPriFields(new ArrayList<myfield>());
+				zt.setCommFields(new ArrayList<myfield>());
+				zm = Pattern
+						.compile(
+								"\\s*`([^`]+)`\\s*([^`]+?)(\\s+|\\([\\d\\s,]+\\))[^`]+?COMMENT\\s*'([^`]+)'\\s*(?:,|\\))")
+						.matcher(TableCon);
+				while (zm.find())
+				{
+					TableCon = zm.group(0).trim();
+					myfield zf = new myfield();
+					zf.setFieldName(zm.group(1).trim());
+					zf.setFDBType(zm.group(2).trim().toLowerCase());
+					zf.setFieldComment(zm.group(4).trim());
+					if (zm.group(4).matches(".*NOT\\s*NULL.*"))
+					{
+						zf.setFieldNotNull(true);
+					}
+					else
+					{
+						zf.setFieldNotNull(false);
+					}
+					boolean ispri = false;
+					for (String pstr : prilist)
+					{
+						if (pstr.equals(zf.getFieldName()))
+						{
+							ispri = true;
+							break;
+						}
+					}
+					if (ispri)
+					{
+						zt.getPriFields().add(zf);
+					}
+					else
+					{
+						zt.getCommFields().add(zf);
+					}
+					zt.getFields().add(zf);
+				}
+				return zt;
+			}
+		}
+		else
+		{
+			AppLog.WriteLog("发生错误：没有找到表数据！", true);
+			return null;
+		}
+	}
+
+	/**
+	 * 检查配置节点是否为空，配置节点为目录的检查目录是否存在
+	 */
+	private static String VNode(Element zroot, String NodeName) throws Exception
+	{
+		Element nownode = zroot.element(NodeName);
+		String StPr = nownode.getText().trim();
+		if (StringUtils.isBlank(StPr))
+		{
+			throw new Exception(NodeName + " 为空！");
+		}
+		else
+		{
+			if (nownode.attribute("ConType") != null)
+			{
+				if ("dir".equals(nownode.attribute("ConType").getValue()))
+				{
+					File prodir = new File(StPr);
+					if (!prodir.exists())
+					{
+						throw new Exception(NodeName + "：" + StPr + " 目录不存在！");
+					}
+				}
+			}
+			AppLog.WriteLog(NodeName + "： " + StPr);
+			return StPr;
+		}
+	}
+
+}
---
+++ src/main/java/com/bookool/MyBatis3AutoCode/myfield.java
@@ -0,0 +1,300 @@
+package com.bookool.MyBatis3AutoCode;
+
+public class myfield
+{
+
+	/**
+	 * 字段名
+	 */
+	private String FieldName;
+
+	/**
+	 * 字段名在Model中的全小写名称
+	 */
+	private String FieldModelName;
+
+	/**
+	 * 字段名在Model中Get\Set时的首字母大写名称
+	 */
+	private String FieldGetSetName;
+
+	/**
+	 * 字段注释
+	 */
+	private String FieldComment;
+
+	/*
+	 * 字段可以为空
+	 */
+	private Boolean FieldNotNull;
+
+	/**
+	 * MySql中的类型名(必须全小写)
+	 */
+	private String FDBType;
+
+	/**
+	 * jdbc中的类型名
+	 */
+	private String FJDBCType;
+
+	/*
+	 * Model中的类型名(全)
+	 */
+	private String FModleTypeFull;
+
+	/*
+	 * Model中的类型名(简)
+	 */
+	private String FModleType;
+
+	public String getFieldName()
+	{
+		return FieldName;
+	}
+
+	public void setFieldName(String fieldName)
+	{
+		FieldName = fieldName;
+		SetFieldName();
+	}
+
+	public String getFieldModelName()
+	{
+		return FieldModelName;
+	}
+
+//	public void setFieldModelName(String fieldModelName)
+//	{
+//		FieldModelName = fieldModelName;
+//	}
+
+	public String getFieldGetSetName()
+	{
+		return FieldGetSetName;
+	}
+
+//	public void setFieldGetSetName(String fieldGetSetName)
+//	{
+//		FieldGetSetName = fieldGetSetName;
+//	}
+
+	public String getFieldComment()
+	{
+		return FieldComment;
+	}
+
+	public void setFieldComment(String fieldComment)
+	{
+		FieldComment = fieldComment;
+	}
+
+	public Boolean getFieldNotNull()
+	{
+		return FieldNotNull;
+	}
+
+	public void setFieldNotNull(Boolean fieldNotNull)
+	{
+		FieldNotNull = fieldNotNull;
+	}
+
+	public String getFDBType()
+	{
+		return FDBType;
+	}
+
+	public void setFDBType(String fDBType) throws Exception
+	{
+		FDBType = fDBType;
+		SetTypeByDBType();
+	}
+
+	public String getFJDBCType()
+	{
+		return FJDBCType;
+	}
+
+//	public void setFJDBCType(String fJDBCType)
+//	{
+//		FJDBCType = fJDBCType;
+//	}
+
+	public String getFModleTypeFull()
+	{
+		return FModleTypeFull;
+	}
+
+//	public void setFModleTypeFull(String fModleTypeFull)
+//	{
+//		FModleTypeFull = fModleTypeFull;
+//	}
+
+	public String getFModleType()
+	{
+		return FModleType;
+	}
+
+//	public void setFModleType(String fModleType)
+//	{
+//		FModleType = fModleType;
+//	}
+
+	/**
+	 * 设置字段名
+	 */
+	private void SetFieldName()
+	{
+		this.FieldModelName = this.FieldName.toLowerCase();
+		String zstr0 = this.FieldModelName.substring(0, 1).toUpperCase();
+		String zstr = this.FieldModelName.substring(1);
+		this.FieldGetSetName = zstr0 + zstr;
+	}
+
+	/**
+	 * 通过数据库类型名，设置其他类型名
+	 */
+	private void SetTypeByDBType() throws Exception
+	{
+		switch (this.FDBType)
+		{
+			case "tinyint":
+				this.FJDBCType = "TINYINT";
+				this.FModleType = "Byte";
+				this.FModleTypeFull = "java.lang.Byte";
+				break;
+			case "smallint":
+				this.FJDBCType = "SMALLINT";
+				this.FModleType = "Short";
+				this.FModleTypeFull = "java.lang.Short";
+				break;
+			case "mediumint":
+				this.FJDBCType = "INTEGER";
+				this.FModleType = "Integer";
+				this.FModleTypeFull = "java.lang.Integer";
+				break;
+			case "int":
+				this.FJDBCType = "INTEGER";
+				this.FModleType = "Integer";
+				this.FModleTypeFull = "java.lang.Integer";
+				break;
+			case "integer":
+				this.FJDBCType = "INTEGER";
+				this.FModleType = "Integer";
+				this.FModleTypeFull = "java.lang.Integer";
+				break;
+			case "bigint":
+				this.FJDBCType = "BIGINT";
+				this.FModleType = "Long";
+				this.FModleTypeFull = "java.lang.Long";
+				break;
+			case "bit":
+				this.FJDBCType = "BIT";
+				this.FModleType = "Boolean";
+				this.FModleTypeFull = "java.lang.Boolean";
+				break;
+			case "real":
+				this.FJDBCType = "DOUBLE";
+				this.FModleType = "Double";
+				this.FModleTypeFull = "java.lang.Double";
+				break;
+			case "double":
+				this.FJDBCType = "DOUBLE";
+				this.FModleType = "Double";
+				this.FModleTypeFull = "java.lang.Double";
+				break;
+			case "float":
+				this.FJDBCType = "REAL";
+				this.FModleType = "Float";
+				this.FModleTypeFull = "java.lang.Float";
+				break;
+			case "decimal":
+				this.FJDBCType = "DECIMAL";
+				this.FModleType = "Long";
+				this.FModleTypeFull = "java.lang.Long";
+				break;
+			case "numeric":
+				this.FJDBCType = "DECIMAL";
+				this.FModleType = "Long";
+				this.FModleTypeFull = "java.lang.Long";
+				break;
+			case "char":
+				this.FJDBCType = "CHAR";
+				this.FModleType = "String";
+				this.FModleTypeFull = "java.lang.String";
+				break;
+			case "varchar":
+				this.FJDBCType = "VARCHAR";
+				this.FModleType = "String";
+				this.FModleTypeFull = "java.lang.String";
+				break;
+			case "date":
+				this.FJDBCType = "DATE";
+				this.FModleType = "Date";
+				this.FModleTypeFull = "java.util.Date";
+				break;
+			case "time":
+				this.FJDBCType = "TIME";
+				this.FModleType = "Date";
+				this.FModleTypeFull = "java.util.Date";
+				break;
+			case "year":
+				this.FJDBCType = "DATE";
+				this.FModleType = "Date";
+				this.FModleTypeFull = "java.util.Date";
+				break;
+			case "timestamp":
+				this.FJDBCType = "TIMESTAMP";
+				this.FModleType = "Date";
+				this.FModleTypeFull = "java.util.Date";
+				break;
+			case "datetime":
+				this.FJDBCType = "TIMESTAMP";
+				this.FModleType = "Date";
+				this.FModleTypeFull = "java.util.Date";
+				break;
+			case "tinytext":
+				this.FJDBCType = "VARCHAR";
+				this.FModleType = "String";
+				this.FModleTypeFull = "java.lang.String";
+				break;
+			case "tinyblob":
+				this.FJDBCType = "BINARY";
+				this.FModleType = "byte[]";
+				this.FModleTypeFull = "";
+				break;
+			case "blob":
+				this.FJDBCType = "LONGVARBINARY";
+				this.FModleType = "byte[]";
+				this.FModleTypeFull = "";
+				break;
+			case "mediumblob":
+				this.FJDBCType = "LONGVARBINARY";
+				this.FModleType = "byte[]";
+				this.FModleTypeFull = "";
+				break;
+			case "longblob":
+				this.FJDBCType = "LONGVARBINARY";
+				this.FModleType = "byte[]";
+				this.FModleTypeFull = "";
+				break;
+			case "text":
+				this.FJDBCType = "LONGVARCHAR";
+				this.FModleType = "String";
+				this.FModleTypeFull = "java.lang.String";
+				break;
+			case "mediumtext":
+				this.FJDBCType = "LONGVARCHAR";
+				this.FModleType = "String";
+				this.FModleTypeFull = "java.lang.String";
+				break;
+			case "longtext":
+				this.FJDBCType = "LONGVARCHAR";
+				this.FModleType = "String";
+				this.FModleTypeFull = "java.lang.String";
+				break;
+			default:
+				throw new Exception("没有找到 [" + this.FDBType + "]对应的类型名！");
+		}
+	}
+}
